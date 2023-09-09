99f0628ba60085466313a23fc2f5968e15c3d8fb
Dmitry F <dimfroloff@gmail.com>
2023-09-09 13:21:57 +0300

    Selenoid added

--- src/test/java/com/jsd/letterboxd/TestBase.java
+++ src/test/java/com/jsd/letterboxd/TestBase.java
@@ -5,6 +5,9 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
 
@@ -12,8 +15,19 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://letterboxd.com";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
