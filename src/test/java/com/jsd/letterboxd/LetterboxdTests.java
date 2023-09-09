8003ca591a7c7c83eb5f83eb234b2727b7d38617
Dmitry F <dimfroloff@gmail.com>
2023-09-09 11:08:07 +0300

    Annotation added

--- src/test/java/com/jsd/letterboxd/LetterboxdTests.java
+++ src/test/java/com/jsd/letterboxd/LetterboxdTests.java
@@ -1,12 +1,19 @@
 package com.jsd.letterboxd;
 
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 public class LetterboxdTests extends TestBase {
 
     @Test
-    void searchTestWithSteps() {
+    @DisplayName("Search test")
+    @Owner("Dmitry F")
+    @Severity(SeverityLevel.CRITICAL)
+    void searchTest() {
         String searchTerm = "Lord of the rings";
         String expectedHeadingText = "FOUND AT LEAST 250 MATCHES FOR “LORD OF THE RINGS”";
 
