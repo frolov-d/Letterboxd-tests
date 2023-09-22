5d17b35563229882fd316d924cf85fa50764628d
Dmitry F <dimfroloff@gmail.com>
2023-09-23 00:43:06 +0300

    small @Tag fix

--- src/test/java/com/jsd/letterboxd/Tests/InventoryTests.java
+++ src/test/java/com/jsd/letterboxd/Tests/InventoryTests.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-@Tag("Web")
+@Tag("web")
 public class InventoryTests extends TestBase {
 
     @Feature("Web tests")
